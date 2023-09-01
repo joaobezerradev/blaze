89647f7c0e2038d4bd1d5d794f1201a39053362f
Joao Bezerra <vitor.lira98@gmail.com>
2023-09-01 19:41:36 -0300

    fix: request props

--- src/request.ts
+++ src/request.ts
@@ -1,60 +1,119 @@
-/* eslint-disable @typescript-eslint/consistent-type-assertions */
 import { IncomingMessage } from 'node:http'
 
 import { type Response } from './response'
 
-// Classe Request que estende IncomingMessage do Node.js
 export class Request extends IncomingMessage {
-  public params: object = {}
-  public query: object = {}
-  public body: object = {}
-  public user: object = {}
+  params: any = {}
+  query: any = {}
+  body: any = {}
+  user: any = {}
+  file: Record<string, { toBuffer: () => Promise<Buffer> }> = {}
 
-  // Método estático para criar uma nova instância de Request
   public static async create (req: IncomingMessage, res: Response): Promise<Request> {
     const request = new Request(req)
     await request.initialize(req, res)
     return request
   }
 
-  // Construtor privado para instanciar a classe
   private constructor (incomingMessage: IncomingMessage) {
     super(incomingMessage.socket)
     Object.assign(this, incomingMessage)
   }
 
-  // Método para inicializar a instância
   private async initialize (req: IncomingMessage, res: Response): Promise<void> {
     await this.parseBodyIfNecessary(req, res)
     this.parseQueryParams(req)
   }
 
-  // Verifica se a análise do corpo da requisição é necessária e a executa, se necessário
   private async parseBodyIfNecessary (req: IncomingMessage, res: Response): Promise<void> {
     if (this.isBodyParsingNecessary(req.method)) {
       await this.parseBody(req, res)
     }
   }
 
-  // Determina se a análise do corpo da requisição é necessária com base no método HTTP
   private isBodyParsingNecessary (method?: string): boolean {
     return ['POST', 'PUT', 'PATCH'].includes(method ?? '')
   }
 
-  // Analisa o corpo da requisição
   private async parseBody (req: IncomingMessage, res: Response): Promise<void> {
-    let body = ''
-    try {
-      for await (const chunk of req) {
-        body += chunk.toString()
+    if (this.isMultipartFormData(req)) {
+      await this.parseMultipartFormData(req, res)
+    } else {
+      let body = Buffer.alloc(0)
+      try {
+        for await (const chunk of req) {
+          body = Buffer.concat([body, chunk])
+        }
+        this.body = JSON.parse(body.toString())
+      } catch (err) {
+        res.internalServerError(err) // Assume this method sends a 500 Internal Server Error
       }
-      this.body = JSON.parse(body)
-    } catch (err) {
-      res.internalServerError(err)
     }
   }
 
-  // Analisa os parâmetros da consulta
+  private isMultipartFormData (req: IncomingMessage): boolean {
+    const contentType = req.headers['content-type'] ?? ''
+    return contentType.startsWith('multipart/form-data')
+  }
+
+  private async parseMultipartFormData (req: IncomingMessage, res: Response): Promise<void> {
+    try {
+      const contentType = req.headers['content-type']!
+      const boundary = contentType.split('boundary=')[1]
+      const delimiter = `--${boundary}`
+      const closeDelimiter = `${delimiter}--`
+
+      let buffer = Buffer.alloc(0)
+
+      return new Promise<void>((resolve, reject) => {
+        req.on('data', (chunk) => {
+          buffer = Buffer.concat([buffer, chunk])
+          let start = 0
+          let end = buffer.indexOf(delimiter)
+
+          while (end !== -1) {
+            if (buffer.slice(start, end).toString() === closeDelimiter) {
+              resolve()
+              return
+            }
+
+            const part = buffer.slice(start, end)
+            this.processPart(part)
+
+            buffer = buffer.slice(end + delimiter.length)
+            start = 0
+            end = buffer.indexOf(delimiter)
+          }
+        })
+
+        req.on('end', () => {
+          if (buffer.length > 0) {
+            this.processPart(buffer)
+          }
+          resolve()
+        })
+
+        req.on('error', reject)
+      })
+    } catch (error) {
+      res.internalServerError(error)
+    }
+  }
+
+  private processPart (part: Buffer): void {
+    const [header, body] = part.toString().split('\r\n\r\n')
+
+    if (!header || !body) return
+
+    const nameMatch = header.match(/name="(.+?)"/)
+    if (!nameMatch) return
+
+    const name = nameMatch[1]
+    this.file[name] = {
+      toBuffer: async () => Promise.resolve(Buffer.from(body))
+    }
+  }
+
   private parseQueryParams (req: IncomingMessage): void {
     const url = new URL(req.url!, `http://${req.headers.host}`)
     this.query = Object.fromEntries(url.searchParams) as any
@@ -62,6 +121,5 @@ export class Request extends IncomingMessage {
 }
 
 export namespace Request {
-  // Define um tipo genérico para as entradas de Request
   export type RequestInput = Record<string, any>
 }
